febd0f2e3b51a2cc843b9da1bc3c9aba6ecbbe7e
vikrant singh <vikrantsinghan5@gmail.com>
2025-08-14 19:39:29 +0530

    ticket done

--- date.js
+++ date.js
@@ -7,12 +7,29 @@ function formatDate(date) {
     
     return `${dd}/${mm}/${yyyy} ${hh}:${min}`;
 }
+function onlyformatDate(date) {
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0');
+    let yyyy = date.getFullYear();
+
+    
+    return `${dd}/${mm}/${yyyy}`;
+}
 
 function showTimes() {
     let now = new Date();
     let thirtyMinsAgo = new Date(now.getTime() - (30 * 60 * 1000));
     return formatDate(thirtyMinsAgo);
 }
+function showTimes1() {
+    let now = new Date();
+    let thirtyMinsAgo = new Date(now.getTime() - (30 * 60 * 1000));
+    return onlyformatDate(thirtyMinsAgo);
+}
 
 // Insert into HTML
-document.querySelector("#date").textContent = "BOOKING DATE: "+ showTimes();
+// document.querySelector("#date").textContent = "BOOKING DATE: "+ showTimes();
+// document.querySelector("#date1").textContent = "BOOKING DATE: "+ showTimes();
+document.querySelector("#date1").textContent = showTimes();
+document.querySelector("#date2").textContent = showTimes1();
+
---
+++ date1.js
@@ -0,0 +1,17 @@
+function formatDate(date) {
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0');
+    let yyyy = date.getFullYear();
+    let hh = String(date.getHours()).padStart(2, '0');
+    let min = String(date.getMinutes()).padStart(2, '0');
+    
+    return `${dd}/${mm}/${yyyy} ${hh}:${min}`;
+}
+
+function showTimes() {
+    let now = new Date();
+    let thirtyMinsAgo = new Date(now.getTime() - (30 * 60 * 1000));
+    return formatDate(thirtyMinsAgo);
+}
+
+document.querySelector("#date").textContent = showTimes();
